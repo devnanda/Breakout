bdb639f762883aba524497e8e734cbea9dceaf44
Devnanda Manjunadh <mdevnanda@gmail.com>
2021-03-31 10:42:38 -0500

    created 3 rows of bricks

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -126,10 +126,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //Now figure the number and spacing of each row of bricks
         let count = Int(frame.width) / 55   // bricks per row
         let xOffset = (Int(frame.width) - (count * 55)) / 2 + Int(frame.minX) + 25
-        let y = Int(frame.maxY) - 15
-        for i in 0..<count {
-            let x = i * 55 + xOffset
-            makeBrick(x: x , y: y, color: .green)
+        
+        let colors: [UIColor] = [.blue, .orange, .green]
+        for r in 0..<3 {
+            let y = Int(frame.maxY) - 15 - (r * 25)
+            for i in 0..<count {
+                let x = i * 55 + xOffset
+                makeBrick(x: x , y: y, color: colors[r])
+            }
         }
     }
     
@@ -199,10 +203,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 contact.bodyB.node == brick {
                 score += 1
                 updateLabels()
-                brick.removeFromParent()
-                removedBricks += 1
-                if removedBricks == bricks.count {
-                    gameOver(winner: true)
+                if brick.color == .blue{
+                    brick.color = .orange //blue bricks turn orange when hit
+                }
+                else if brick.color == .orange {
+                    brick.color = .green //orange bricks turn green
+                }
+                else {
+                    brick.removeFromParent()
+                    removedBricks += 1
+                    if removedBricks == bricks.count {
+                        gameOver(winner: true)
+                    }
                 }
             }
         }
